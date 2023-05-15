4a0840bbadf83b433ab5c110505c8e622c9c8d07
mskian <10300271+mskian@users.noreply.github.com>
2023-05-15 11:53:22 +0530

    new changes

--- _data/site.js
+++ _data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   meta: {
-    title: "படித்ததில் பிடித்தது",
-    description: "Collect the Favourite Tamil kavithai and Tamil Quotes From Tamil Writers Social Profile.",
+    title: "Tamil Status - படித்ததில் பிடித்தது",
+    description: "Tamil SMS Blog - Here I Collect the Favourite Tamil Kavithai, Tamil Quotes Tamil Status, and Kadhal Kavithai From Tamil Writer's Online Social Profile.",
     lang: "en",
     siteUrl: "https://status.tamilsms.blog",
     facebook: "https://www.facebook.com/tamilsmsblog",
